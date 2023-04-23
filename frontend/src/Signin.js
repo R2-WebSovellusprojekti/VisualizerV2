2e0db44d97dac20cb9abef2d222247336524564a
Niko Kolehmainen <niko.kolehmainen@windowslive.com>
2023-04-23 22:12:28 +0300

    render test

--- frontend/src/Signin.js
+++ frontend/src/Signin.js
@@ -7,7 +7,7 @@ function SignInForm() {
   const [passwordError, setPasswordError] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [successMessage, setSuccessMessage] = useState('');
-  const SERVER_ADDRESS = 'https://r2-websovellusprojekti.onrender.com/api/signin';
+  const SERVER_ADDRESS = 'postgres://r2:So89P9cm37yaR22nqNjyktWJLSB4Ywo7@dpg-cgi5v4seoogvqrjl6amg-a/r2db/api/signin';
 
   function handleUsernameChange(event) {
     setUsername(event.target.value);
